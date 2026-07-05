0cc9b5efd2e773a9a5bbe1e59624b4e85832308c
krfx502 <mihaikenj@gmail.com>
2026-07-05 14:10:22 +0300

    update

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include "SignalBot.hpp"
+#include <cstdlib>
+#include <iostream>
 
 int main() {
+	const char* phone_number = std::getenv("SIGNAL_PHONE");
+
+	if (!phone_number) {
+		std::cout << "ERROR: Cannot find the phone number!";
+		return 1;
+	}
+
 	SignalBot marcel;
 
-	marcel.phoneNumber = "+40701542197";
+	marcel.phoneNumber = phone_number;
 	marcel.listen();
 
 	return 0;
